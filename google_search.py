d7e2e0b773369252c614877cfa2f67f482b72e73
Badr El Mazaz <59953925+Fermin-Romero-De-Torres@users.noreply.github.com>
2021-10-12 23:32:48 +0200

    pythonify

--- google_search.py
+++ google_search.py
@@ -1,6 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
-from random_user_agent.user_agent import UserAgent
+# Standard Python libraries.
 from enum import Enum
 from typing import Optional
 import urllib.parse
@@ -8,10 +6,14 @@ import sys
 import os.path as op
 import json
 
+# Third party Python libraries.
+from bs4 import BeautifulSoup
+import requests
 
 
+__version__="0.0.1"
 
-# advanced search
+
 class QueryType(Enum):
     ALL_THESE_WORDS_PARAMETER_DESCRIPTION = "Type the important words: tri-colour rat terrier"
     THESE_EXACT_WORDS_PARAMETER_DESCRIPTION = "Put exact words in quotes: \"rat terrier\""
@@ -36,15 +38,16 @@ class Query():
 class Language():
     DESCRIPTION = "Find pages in the language that you select."
     PARAMETER = "lr"
+
     def __init__(self, language_code: Optional[str] = "eng"):
         self.language_code = language_code.lower()
-        f=open(op.join("resources", "languages.json"))
+        f = open(op.join("resources", "languages.json"))
 
-        data=json.load(f)
+        data = json.load(f)
         f.close()
-        if self.language_code!="eng":
+        if self.language_code != "eng":
             for item in data:
-                if item["code"]==self.language_code:
+                if item["code"] == self.language_code:
                     break
             else:
                 print("Language not found, eng will set by default")
@@ -63,12 +66,11 @@ class Region():
         if self.region_code != "":
             for item in data:
                 if item["code"] == self.region_code:
-                    self.region_code="country"+region_code
+                    self.region_code = "country" + region_code
                     break
             else:
                 print("Region not found, your ip region will be set")
 
-    # todo put all countries
 
 
 class LastUpdate(Enum):
@@ -144,11 +146,10 @@ def search(query: Query,
            usage_right: Optional[UsageRight] = UsageRight.NOT_FILTERED_BY_LICENSE,
            num_results: Optional[int] = 10,
            proxy=None):
-    user_agent_rotator = UserAgent()
+
 
     # encode url with %values
     escaped_search_query = urllib.parse.quote(query.query).replace('%20', '+')
-
 
     google_url = f'https://www.google.com/search?{query.query_type.value}={escaped_search_query}&' \
                  f'num={num_results + 1}&' \
@@ -161,25 +162,19 @@ def search(query: Query,
                  f'{FileType.PARAMETER.value}={file_type.value}&' \
                  f'{UsageRight.PARAMETER.value}={usage_right.value}'
 
-
-    def fetch_results(search_query):
+    def fetch_results():
         proxies = None
         if proxy:
             if proxy[:5] == "https":
                 proxies = {"https": proxy}
             else:
                 proxies = {"http": proxy}
-        usr_agent = {
+        user_agent = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/61.0.3163.100 Safari/537.36'}
 
-        #todo fix user agent rotator
-        # response = get(google_url, proxies=proxies, headers={'User-Agent': user_agent_rotator.get_random_user_agent()})
-        usr=user_agent_rotator
-        print(usr)
         try:
-            # response = requests.get(google_url, proxies=proxies, headers=usr_agent)
-            response = requests.get(google_url, proxies=proxies,headers={'User-Agent': usr})
+            response = requests.get(google_url, proxies=proxies, headers={'User-Agent': user_agent})
             response.raise_for_status()
         except requests.exceptions.HTTPError as errh:
             print("Http Error:", errh)
@@ -190,14 +185,12 @@ def search(query: Query,
         except requests.exceptions.RequestException as err:
             print("Unkown Error, try using proxy", err)
 
-
         return response.text
 
     def parse_results(raw_html):
         soup = BeautifulSoup(raw_html, 'html.parser')
         result_block = soup.find_all('div', attrs={'class': 'g'})
         for result in result_block:
-
             link = result.find('a', href=True)
             title = result.find('h3')
             if link and title:
@@ -208,4 +201,5 @@ def search(query: Query,
 
 
 if __name__ == '__main__':
-    print(search(query=Query(sys.argv[1], QueryType.ALL_THESE_WORDS_PARAMETER), num_results=int(sys.argv[3]), language=Language(sys.argv[2])))
+    print(search(query=Query(sys.argv[1], QueryType.ALL_THESE_WORDS_PARAMETER), num_results=int(sys.argv[3]),
+                 language=Language(sys.argv[2])))
